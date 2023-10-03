8c6e64e23ba9c0c3769e6c18e6d55a942b8bfa21
Bea-Code-maker <146862448+Bea-Code-maker@users.noreply.github.com>
2023-10-03 18:48:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const semafor = document.getElementById("semafor");
+let culoare = 0;
+semafor.addEventListener("click",function(){
+    if(culoare === 0) {
+        semafor.className ="btn btn-warning";
+        semafor.innerText = "Galben";
+        culoare = 1;
+    } else if(culoare === 1) {
+        semafor.className = "btn btn-success";
+        semafor.innerText = "Verde";
+        culoare = 2; 
+    } else {
+        semafor.className = "btn btn-danger";
+        semafor.innerText = "Rosu";
+        culoare = 0;
+    }
+});
